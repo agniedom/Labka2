b7cb69a3cc98dbd349e34f2e04efcb38feeb9719
unknown <student@IBLAB732P-10.ib.polsl.pl>
2018-01-03 15:00:23 +0100

    wszystko do zad3

--- app/src/main/java/com/example/student/aplikacjapogodowa2/MainActivity.java
+++ app/src/main/java/com/example/student/aplikacjapogodowa2/MainActivity.java
@@ -7,48 +7,74 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.txt_edittext)
+    EditText Edit_WpiszMiasto ;
+
+    @BindView(R.id.btn_zmien_miejsce)
+    Button btn_zmienMiejsce ;
+
+    @BindView(R.id.btn_pokaz_liste_dostepnych_miejsc)
+    Button btn_lista ;
+
+    @BindView(R.id.txt_miasto)
+    TextView txt_NazwaMiasta ;
+
+    @BindView(R.id.obrazek)
+    ImageView obrazek;
+
+    @OnClick(R.id.btn_zmien_miejsce)
+    void onClick(View view){
+
+        String nowe = Edit_WpiszMiasto.getText().toString();
+        txt_NazwaMiasta.setText(nowe);
+    }
+    @OnClick(R.id.btn_pokaz_liste_dostepnych_miejsc)
+    void onClick2(View view){
+
+        Intent intent = new Intent( MainActivity.this, Main2Activity.class);
+        Bundle bundle = new Bundle();
+        String wpisanyTekst = Edit_WpiszMiasto.getText().toString();
+        bundle.putString("KEY", wpisanyTekst);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
-        Button btn2 = findViewById(R.id.btn_pokaz_liste_dostepnych_miejsc);
+        ButterKnife.bind(this);
 
-        Button btn1 = findViewById(R.id.btn_zmien_miejsce);
-
-
-        final EditText editText = findViewById(R.id.txt_edittext);
-        final TextView miasto = findViewById(R.id.txt_miasto);
-        
-        btn2.setOnClickListener(new View.OnClickListener()
-
-        {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(intent);
-
-            }
+        Glide.with(this)
+                .load("https://www.krak-wit.pl/userdata/gfx/45c1f236531e5a40bafa43ff1903312e.jpg")
+                .into(obrazek);
 
 
-        });
-        btn1.setOnClickListener(new View.OnClickListener()
-
-        {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                String g = editText.getText().toString();
-                miasto.setText(g);
-                Bundle bundle = new Bundle();
-                bundle.putString("item", g);
-                intent.putExtras(bundle);
-
-            }
 
 
-        });
-    }}
+        Intent getData = getIntent();
+        Bundle bundle = getIntent().getExtras();
+
+
+        if (bundle != null) {
+            String place = bundle.getString("Nazwa");
+            txt_NazwaMiasta.setText(place);
+        }
+
+
+
+
+    }
+
+}
